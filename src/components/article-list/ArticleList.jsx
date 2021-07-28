daa632473c9479da5f2bfa11298d31abceeb57aa
Chase <cabbott93@gmail.com>
2021-07-28 15:34:53 -0700

    ArticleList tests pass

---
+++ src/components/article-list/ArticleList.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Article from '../article/Article.jsx';
+
+const ArticleList = ({ articles }) => {
+  const articleItems = articles.map(article => (
+    <li key={article.title}>
+      <Article article={article}/>
+    </li>
+  ));
+
+  return <ul> {articleItems}</ul>;
+};
+
+ArticleList.propTypes = {
+  articles: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      author: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired
+    })
+  )
+};
+
+export default ArticleList;
